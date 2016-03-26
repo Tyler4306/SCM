ac2589b53343c19d27c0cc39b54bf4603b323f1d
humamAbedrabbo <humam.abedrabbo@hotmail.com>
2016-03-26 14:52:59 +0200

    Add PendingReasons

---
+++ SCM/Controllers/PendingReasonsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SCM;
+using PagedList;
+using System.Linq.Dynamic;
+
+namespace SCM.Controllers
+{
+    public class PendingReasonsController : Controller
+    {
+        private SCMContext db = new SCMContext();
+
+        public ActionResult Index(int? page = null)
+        {
+            var model = db.PendingReasons.ToList();
+            int pageNumber = page ?? 1;
+            int pageSize = 10;
+            return View(model.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult List(int? page = null, string name = null, string sortBy = "Reason", string direction = "ASC")
+        {
+            var model = db.PendingReasons.Where(x => string.IsNullOrEmpty(name) || x.Reason.Contains(name)).OrderBy(sortBy + " " + direction).ToList();
+            int pageNumber = page ?? 1;
+            int pageSize = 10;
+            return PartialView(model.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var model = db.PendingReasons.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Create([Bind(Include = "Id,Reason")] PendingReason model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PendingReasons.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var model = db.PendingReasons.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Edit([Bind(Include = "Id,Reason")] PendingReason model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var model = db.PendingReasons.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var model = db.PendingReasons.Find(id);
+            db.PendingReasons.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
