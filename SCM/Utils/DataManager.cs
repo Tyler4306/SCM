7b45bf475c98199c2407af19b1e4623af55c59fe
humamAbedrabbo <humam.abedrabbo@hotmail.com>
2016-03-29 12:10:19 +0200

    Remove fixed 2015 from criteria

--- SCM/Utils/DataManager.cs
+++ SCM/Utils/DataManager.cs
@@ -29,7 +29,7 @@ namespace SCM.Utils
 
                     var qry = (from x in ctx.ServiceRequests.Include(x => x.Customer).Include(x => x.Product).Include(x => x.Tags).Include(x => x.Customer.Tags)
                                where (x.StatusId < 90 && !x.IsDeleted) ||
-                                (x.StatusId >= 90 && x.ClosingDate.HasValue && x.ClosingDate.Value.Year >= 2015 && !x.IsDeleted)
+                                (x.StatusId >= 90 && x.ClosingDate.HasValue && x.ClosingDate.Value.Year == DateTime.Today.Year && !x.IsDeleted)
                                orderby x.RequestDate descending
                                select x);
 
