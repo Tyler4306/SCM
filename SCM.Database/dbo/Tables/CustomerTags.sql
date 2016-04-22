231351613abb0edf12e8b5ecc4158bb7cd2810b2
humamAbedrabbo <humam.abedrabbo@hotmail.com>
2016-04-22 20:50:40 +0300

    delete cascade added

--- SCM.Database/dbo/Tables/CustomerTags.sql
+++ SCM.Database/dbo/Tables/CustomerTags.sql
@@ -3,6 +3,6 @@
 	[TagId] INT NOT NULL , 
     [CustomerId] INT NOT NULL, 
     CONSTRAINT [PK_CustomerTags] PRIMARY KEY ([TagId], [CustomerId]), 
-    CONSTRAINT [FK_CustomerTags_To_Customers] FOREIGN KEY (CustomerId) REFERENCES Customers(Id), 
-    CONSTRAINT [FK_CustomerTags_To_Tags] FOREIGN KEY (TagId) REFERENCES Tags(Id)
+    CONSTRAINT [FK_CustomerTags_To_Customers] FOREIGN KEY (CustomerId) REFERENCES Customers(Id) ON DELETE CASCADE, 
+    CONSTRAINT [FK_CustomerTags_To_Tags] FOREIGN KEY (TagId) REFERENCES Tags(Id) ON DELETE CASCADE
 )
