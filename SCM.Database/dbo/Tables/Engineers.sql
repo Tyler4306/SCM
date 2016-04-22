95edd9653e1836b49ad317ba788a8aa02c81597f
humamAbedrabbo <humam.abedrabbo@hotmail.com>
2016-04-22 20:51:05 +0300

    IsActive field added, delete action set default

--- SCM.Database/dbo/Tables/Engineers.sql
+++ SCM.Database/dbo/Tables/Engineers.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[Engineers]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [DepartmentId] INT NOT NULL, 
+    [DepartmentId] INT NOT NULL DEFAULT 1, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Phone] NVARCHAR(20) NULL, 
-    CONSTRAINT [FK_Engineers_To_Departments] FOREIGN KEY (DepartmentId) REFERENCES Departments(Id)
+    [IsActive] BIT NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_Engineers_To_Departments] FOREIGN KEY (DepartmentId) REFERENCES Departments(Id) ON DELETE SET DEFAULT
 )
 
 GO
