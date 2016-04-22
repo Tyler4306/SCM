0e734bd4db09ff97e1641c638c4f1d63835858bc
humamAbedrabbo <humam.abedrabbo@hotmail.com>
2016-04-22 20:52:38 +0300

    Delete action cascade

--- SCM.Database/dbo/Tables/RequestTags.sql
+++ SCM.Database/dbo/Tables/RequestTags.sql
@@ -3,6 +3,6 @@
 	[TagId] INT NOT NULL , 
     [ServiceRequestId] INT NOT NULL, 
     CONSTRAINT [PK_RequestTags] PRIMARY KEY ([TagId], [ServiceRequestId]), 
-    CONSTRAINT [FK_RequestTags_To_Tags] FOREIGN KEY (TagId) REFERENCES Tags(Id), 
-    CONSTRAINT [FK_RequestTags_To_ServiceRequests] FOREIGN KEY (ServiceRequestId) REFERENCES ServiceRequests(Id)
+    CONSTRAINT [FK_RequestTags_To_Tags] FOREIGN KEY (TagId) REFERENCES Tags(Id) ON DELETE CASCADE, 
+    CONSTRAINT [FK_RequestTags_To_ServiceRequests] FOREIGN KEY (ServiceRequestId) REFERENCES ServiceRequests(Id) ON DELETE CASCADE
 )
