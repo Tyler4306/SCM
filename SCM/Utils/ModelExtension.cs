854f4bfd46d6801059be80f0a80efabf1d779f2d
humamAbedrabbo <humam.abedrabbo@hotmail.com>
2016-05-01 00:23:39 +0300

    phone and mobile format added

--- SCM/Utils/ModelExtension.cs
+++ SCM/Utils/ModelExtension.cs
@@ -13,6 +13,46 @@ namespace SCM.Models
         {
             return string.Format("{2} {1} {0}", (customer.RegionId.HasValue) ? customer.Region.Name : "", customer.Address, (customer.CityId.HasValue) ? customer.City.Name : "").Trim();
         }
+        public static string PhoneFormatted(this Customer customer)
+        {
+            var phone = customer.Phone;
+            if (!string.IsNullOrEmpty(phone))
+            {
+                phone = phone.Trim();
+                if (phone.Length > 4 && (phone.StartsWith("09") || phone.StartsWith("+9")))
+                {
+                    return string.Format("({0})-{1}", phone.Substring(1, 3), phone.Substring(4));
+                }
+                else if (phone.Length > 3 && (phone.StartsWith("0") || phone.StartsWith("+")) && phone[1] != '9' )
+                {
+                    return string.Format("({0})-{1}", phone.Substring(1, 2), phone.Substring(3));
+                }
+                else
+                    return phone;
+            }
+            else
+                return "";
+        }
+        public static string MobileFormatted(this Customer customer)
+        {
+            var phone = customer.Mobile;
+            if (!string.IsNullOrEmpty(phone))
+            {
+                phone = phone.Trim();
+                if (phone.Length > 4 && (phone.StartsWith("09") || phone.StartsWith("+9")))
+                {
+                    return string.Format("({0})-{1}", phone.Substring(1, 3), phone.Substring(4));
+                }
+                else if (phone.Length > 3 && (phone.StartsWith("0") || phone.StartsWith("+")) && phone[1] != '9')
+                {
+                    return string.Format("({0})-{1}", phone.Substring(1, 2), phone.Substring(3));
+                }
+                else
+                    return phone;
+            }
+            else
+                return "";
+        }
         #endregion
 
 
@@ -38,5 +78,29 @@ namespace SCM.Models
             return result;
         }
         #endregion
+
+        #region Engineer Extensions
+        public static string PhoneFormatted(this Engineer engineer)
+        {
+            var phone = engineer.Phone;
+            if (!string.IsNullOrEmpty(phone))
+            {
+                phone = phone.Trim();
+                if (phone.Length > 4 && (phone.StartsWith("09") || phone.StartsWith("+9")))
+                {
+                    return string.Format("({0})-{1}", phone.Substring(1, 3), phone.Substring(4));
+                }
+                else if (phone.Length > 3 && (phone.StartsWith("0") || phone.StartsWith("+")) && phone[1] != '9')
+                {
+                    return string.Format("({0})-{1}", phone.Substring(1, 2), phone.Substring(3));
+                }
+                else
+                    return phone;
+            }
+            else
+                return "";
+        } 
+        #endregion
+
     }
 }
