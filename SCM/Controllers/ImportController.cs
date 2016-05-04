da4b92daaf4fcb2c87f5e7adea8b7a69c4787bc0
humamAbedrabbo <humam.abedrabbo@hotmail.com>
2016-05-05 00:28:13 +0300

    Import controller authorization to admin added

--- SCM/Controllers/ImportController.cs
+++ SCM/Controllers/ImportController.cs
@@ -11,7 +11,7 @@ using System.Web.Mvc;
 
 namespace SCM.Controllers
 { 
-    [Authorize]
+    [Authorize(Roles = "Admin")]
     public class ImportController : Controller
     {
         public static ImportManager manager { get; set; }
